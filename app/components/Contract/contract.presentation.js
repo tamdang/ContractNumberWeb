4feeac7cabdb8cc7df149167fb529ebbccf0ed50
Dang Thanh Tam <dangtam@gmail.com>
2018-03-24 14:49:22 +0700

    Add moment
    Update simple table using .table class

--- app/components/Contract/contract.presentation.js
+++ app/components/Contract/contract.presentation.js
@@ -1,16 +1,38 @@
 import React from 'react'
+import moment from 'moment'
 class ContractPresentation extends React.Component{
+  state = {
+    executive: '',
+    budget: 0,
+    signDate: '2018-03-04',
+    start: Date(),
+    end: Date(),
+    description: ''
+  }
+  constructor(){
+    super()
+    this._onValueChange = this._onValueChange.bind(this)
+  }
+  _onValueChange(field){
+    return event => {
+      this.setState({[field]: event.target.value})
+    }
+  }
   render(){
+    const {executive, budget, signDate, start, end, description} = this.state
+    console.log(signDate)
     return (
       <div>
         <form>
           <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+            <label htmlFor="budget">Budget</label>
+            <input type="number" className="form-control" id="budget" placeholder="Budget" 
+              value={budget} onChange={this._onValueChange('budget')}/>
           </div>
           <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+            <label htmlFor="signDate">Sign Date</label>
+            <input type="date" className="form-control" id="signDate" placeholder="Sign date"
+              valuel={signDate} onChange={this._onValueChange('signDate')}/>
           </div>
           <div className="form-check">
             <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
